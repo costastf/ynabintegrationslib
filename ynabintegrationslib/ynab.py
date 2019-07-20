dcdb244d85201d3dbe56bca12ae00d55fff7479d
Costas Tyfoxylos <costas.tyf@gmail.com>
2019-07-20 12:15:44 +0200

    completed linting

--- ynabintegrationslib/ynab.py
+++ ynabintegrationslib/ynab.py
@@ -34,7 +34,7 @@ Main code for ynab
 import logging
 
 from requests import Session
-from ynabintegrationslib.ynabintegrationslib import InvalidBudget
+from ynabintegrationslib.ynabintegrationslibexceptions import InvalidBudget
 
 __author__ = '''Costas Tyfoxylos <costas.tyf@gmail.com>'''
 __docformat__ = '''google'''
@@ -62,8 +62,6 @@ class Ynab:
         self._base_url = url
         self.api_url = f'{self._base_url}/{self._api_version}'
         self._session = self._get_authenticated_session(token)
-        self._budgets = self.budgets
-        self._default_budget = self.default_budget
 
     def _get_authenticated_session(self, token):
         budget_url = f'{self.api_url}/budgets'
@@ -93,7 +91,7 @@ class Ynab:
             budget (Budget): A budget object on success, None otherwise
 
         """
-        return next((budget for budget in self._budgets if budget.name.lower() == budget_name.lower()), None)
+        return next((budget for budget in self.budgets if budget.name.lower() == budget_name.lower()), None)
 
     def get_accounts_for_budget(self, budget_name):
         """Retrieves the accounts for a budget
@@ -118,6 +116,7 @@ class Ynab:
             account (Account): The account object to upload the transactions to
 
         Returns:
+            boolean (bool): True on success, False otherwise
 
         """
         transaction_url = f'{self.api_url}/budgets/{account.budget.id}/transactions'
@@ -131,6 +130,7 @@ class Ynab:
 
 
 class Budget:
+    """Models the YNAB budget object"""
 
     def __init__(self, ynab, data):
         self._data = data
@@ -138,45 +138,63 @@ class Budget:
 
     @property
     def accounts(self):
+        """Accounts of the budget"""
         url = f'{self._ynab.api_url}/budgets/{self.id}/accounts'
-        response = self._ynab._session.get(url)
+        response = self._ynab._session.get(url)  # pylint: disable=protected-access
         response.raise_for_status()
         return [Account(account, self)
                 for account in response.json().get('data', {}).get('accounts', [])]
 
     @property
     def currency_format(self):
+        """Currency format"""
         return self._data.get('currency_format')
 
     @property
     def date_format(self):
+        """Date format"""
         return self._data.get('date_format')
 
     @property
     def first_month(self):
+        """First month"""
         return self._data.get('first_month')
 
     @property
-    def id(self):
+    def id(self):  # pylint: disable=invalid-name
+        """ID"""
         return self._data.get('id')
 
     @property
     def last_modified_on(self):
+        """Last modified on"""
         return self._data.get('last_modified_on')
 
     @property
     def last_month(self):
+        """Last month"""
         return self._data.get('last_month')
 
     @property
     def name(self):
+        """Name"""
         return self._data.get('name')
 
     def get_account_by_name(self, name):
+        """Retrieves an account by name
+
+        Args:
+            name (str): The name of the account to retireve
+
+        Returns:
+            account (Account): An account object on success, None otherwise.
+
+        """
         return next((account for account in self.accounts if account.name.lower() == name.lower()), None)
 
 
 class Account:
+    """Models the account of a YNAB Budget"""
 
     def __init__(self, data, budget):
         self._data = data
@@ -184,44 +202,55 @@ class Account:
 
     @property
     def budget(self):
+        """Budget"""
         return self._budget
 
     @property
     def balance(self):
+        """Balance"""
         return self._data.get('balance')
 
     @property
     def cleared_balance(self):
+        """Cleared balance"""
         return self._data.get('cleared_balance')
 
     @property
     def closed(self):
+        """Closed"""
         return self._data.get('closed')
 
     @property
     def deleted(self):
+        """Deleted"""
         return self._data.get('deleted')
 
     @property
-    def id(self):
+    def id(self):  # pylint: disable=invalid-name
+        """ID"""
         return self._data.get('id')
 
     @property
     def name(self):
+        """Name"""
         return self._data.get('name')
 
     @property
     def note(self):
+        """Note"""
         return self._data.get('note')
 
     @property
     def on_budget(self):
+        """On budget"""
         return self._data.get('on_budget')
 
     @property
     def transfer_payee_id(self):
+        """Transfer payee ID"""
         return self._data.get('transfer_payee_id')
 
     @property
     def type(self):
+        """Type"""
         return self._data.get('type')
